1ae34bb90df67018911b59b4181b301ffe636073
cox101 <cox2659@gmail.com>
2023-12-16 10:32:18 -0800

    Initial commit

---
+++ pint.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * f_pint - print value at the top of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pint(stack_t **head, unsigned int counter)
+{
+    if (*head == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*head)->n);
+}
+
--- push.c
+++ push.c
@@ -1,39 +1,64 @@
 #include "monty.h"
 
 /**
+ * handle_error - handle errors and exit
+ * @message: error message
+ * @head: stack head
+ * @counter: line number
+ */
+void handle_error(const char *message, stack_t **head, unsigned int counter)
+{
+    fprintf(stderr, message, counter);
+    fclose(bus.file);
+    free(bus.content);
+    free_stack(*head);
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * addnode - add node to the stack
+ * @head: stack head
+ * @n: value to be added
+ */
+void addnode(stack_t **head, int n)
+{
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = n;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+/**
  * f_push - add node to the stack
  * @head: stack head
- * @counter: line_number
+ * @counter: line number
  * Return: no return
  */
-
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+    int n, j = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+    if (!bus.arg)
+        handle_error("L%d: usage: push integer\n", head, counter);
+
+    if (bus.arg[0] == '-')
+        j++;
+
+    for (; bus.arg[j] != '\0'; j++)
+    {
+        if (!isdigit(bus.arg[j]))
+            handle_error("L%d: usage: push integer\n", head, counter);
+    }
+
+    n = atoi(bus.arg);
+    addnode(head, n);
 }
